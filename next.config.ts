a907ee6502d570615efcf2895e7b65956425e398
Techevery <110732668+Techevery@users.noreply.github.com>
2025-07-03 13:24:33 +0100

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['kscuisine.com', "images.unsplash.com",'public.readdy.ai'],
+  },
+};
+
+module.exports = nextConfig;
